18a7d15186e019ab5082fd297c50fde7378e0002
Andrey-Vlasenko <andrey05@yandex.ru>
2016-01-20 15:48:12 +0300

    scripts and grafs

--- plot2.R
+++ plot2.R
@@ -1,12 +1,16 @@
 library(data.table)
-path <- setwd(choose.dir(default = "", caption = "Select folder"))
-png(file="plot2.png",width=480,height=480)
+pathProject <- "C:/Users/vlasenko/YandexDisk/R_Programming/ExData_Plotting1"
+#pathProject <- setwd(choose.dir(getwd(), caption = "Choose folder with the Project"))
+pathData <- pathProject 
+#pathProject <- setwd(choose.dir(getwd(), caption = "Choose folder with the Data"))
+setwd(pathProject)
+png(file="plot2.png",width=480,height=480) 
 
 windows.options(width=480, height=480)
 language <- "English"
 Sys.setlocale("LC_TIME", language) 
 
-data <- fread(paste(path,"\\household_power_consumption.txt",sep=""),sep=";",header=T,na.strings="?")
+data <- fread(paste(pathData,"/household_power_consumption.txt",sep=""),sep=";",header=T,na.strings="?")
 data$Date <- as.Date(data$Date ,"%d/%m/%Y")
 data2 <- subset(data , data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 data2$date2 <- paste(data2$Date, data2$Time, sep = " ")
